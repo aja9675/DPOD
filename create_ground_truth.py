cb17feaadf7f8bc6afe0430dd9786281b98d9e1a
Alex Avery <aja9675@rit.edu>
2020-11-25 16:08:06 -0500

    Fix & improve hole filling
    Can't use infill for id masks, and mask for infilling u and v
    was all zeros.
    
    Further improvements could be made on UV masks, so debugging code
    is left in but commented out.
    
    Signed-off-by: Alex Avery <aja9675@rit.edu>

--- create_ground_truth.py
+++ create_ground_truth.py
@@ -5,21 +5,17 @@ import cv2
 import pickle
 import random
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from helper import save_obj, load_obj
+from helper import *
 
-
+"""
+Helper function to the read the rotation and translation file
+    Args:
+            rot_adr (str): path to the file containing rotation of an object
+    tra_adr (str): path to the file containing translation of an object
+    Returns:
+            rigid transformation (np array): rotation and translation matrix combined
+"""
 def get_rot_tra(rot_adr, tra_adr):
-    """
-    Helper function to the read the rotation and translation file
-        Args:
-                rot_adr (str): path to the file containing rotation of an object
-        tra_adr (str): path to the file containing translation of an object
-        Returns:
-                rigid transformation (np array): rotation and translation matrix combined
-    """
-
     rot_matrix = np.loadtxt(rot_adr, skiprows=1)
     trans_matrix = np.loadtxt(tra_adr, skiprows=1)
     trans_matrix = np.reshape(trans_matrix, (3, 1))
@@ -27,32 +23,62 @@ def get_rot_tra(rot_adr, tra_adr):
 
     return rigid_transformation
 
+"""
+Helper function to fill the holes in id , u and vmasks.
+Note that this only works on masks with a single class id.
+It'd be possible, but much more difficult to fill holes in multiple classes simultaneously.
+This is not necessary for the LineMOD dataset.
+Also note that further improvements could be made to fill in the 'bright' holes. These
+are caused by the sparsity of the point cloud, where you can 'see through' the darker region.
+Args:
+    idmask (uint8 np.array): id mask whose holes you want to fill
+    umask (uint8 np.array): u mask whose holes you want to fill
+    vmask (uint8 np.array): v mask whose holes you want to fill
+    class_id (uint8): class id of object
+Returns:
+    filled_id_mask (np array): id mask with holes filled
+    filled_u_mask (np array): u mask with holes filled
+    filled_id_mask (np array): v mask with holes filled
+"""
+def fill_holes(idmask, umask, vmask, class_id):
+    #showImage("idmask", idmask*(255/np.max(idmask)))
+    #showImage("umask", umask)
+    #showImage("vmask", vmask)
 
-def fill_holes(idmask, umask, vmask):
-    """
-    Helper function to fill the holes in id , u and vmasks
-        Args:
-                idmask (np.array): id mask whose holes you want to fill
-        umask (np.array): u mask whose holes you want to fill
-        vmask (np.array): v mask whose holes you want to fill
-        Returns:
-                filled_id_mask (np array): id mask with holes filled
-        filled_u_mask (np array): u mask with holes filled
-        filled_id_mask (np array): v mask with holes filled
-    """
-    idmask = np.array(idmask, dtype='float32')
-    umask = np.array(umask, dtype='float32')
-    vmask = np.array(vmask, dtype='float32')
-    thr, im_th = cv2.threshold(idmask, 0, 255, cv2.THRESH_BINARY_INV)
+    thr, im_th = cv2.threshold(idmask, 0, 255, cv2.THRESH_BINARY)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    res = cv2.morphologyEx(im_th, cv2.MORPH_OPEN, kernel)
-    im_th = cv2.bitwise_not(im_th)
-    des = cv2.bitwise_not(res)
-    mask = np.array(des-im_th, dtype='uint8')
-    filled_id_mask = cv2.inpaint(idmask, mask, 5, cv2.INPAINT_TELEA)
-    filled_u_mask = cv2.inpaint(umask, mask, 5, cv2.INPAINT_TELEA)
-    filled_v_mask = cv2.inpaint(vmask, mask, 5, cv2.INPAINT_TELEA)
+    mask = cv2.morphologyEx(im_th, cv2.MORPH_CLOSE, kernel)
 
+    # Inpainting won't work for class_id here, it'll just fill it with zeros since the source
+    # image is also a mask
+    filled_id_mask = idmask
+    filled_id_mask[mask > 0] = class_id
+
+    # Now we don't want to inpaint the whole mask region for U and V, but rather the gaps in it
+    umask_gaps = np.uint8(umask == 0)
+    #showImage("umask_gaps", cv2.resize(umask_gaps*255, (1600, 1200)))
+    umask_gaps = cv2.bitwise_and(umask_gaps, umask_gaps, mask=mask)
+    #showImage("umask_gaps2", cv2.resize(umask_gaps*255, (1600, 1200)))
+    filled_u_mask = cv2.inpaint(umask, umask_gaps, 5, cv2.INPAINT_TELEA)
+    # Alternately, we could median filter, this would also fill in the "bright spots"
+    #filled_u_mask = cv2.medianBlur(umask, 5)
+
+    vmask_gaps = np.uint8(vmask == 0)
+    vmask_gaps = cv2.bitwise_and(vmask_gaps, vmask_gaps, mask=mask)
+    filled_v_mask = cv2.inpaint(vmask, vmask_gaps, 5, cv2.INPAINT_TELEA)
+
+    # Check that our filled mask only contains 0 and class_id
+    assert((np.unique(filled_id_mask) == [0, class_id]).all())
+
+    # Debugging - check the residuals of our operation to ensure we're not over-correcting
+    #diff = np.uint8(abs(np.int16(filled_u_mask)-np.int16(umask)))
+    #unique, counts = np.unique(diff, return_counts=True)
+    #print(unique, "\n", counts)
+    #showImage("diff u_mask", cv2.resize(diff, (1600, 1200)))
+
+    #showImage("filled_id_mask", cv2.resize(idmask*(255/np.max(idmask)), (1600, 1200)))
+    #showImage("filled_u_mask", cv2.resize(filled_u_mask, (1600, 1200)))
+    #showImage("filled_v_mask", cv2.resize(filled_v_mask, (1600, 1200)))
     return filled_id_mask, filled_u_mask, filled_v_mask
 
 
@@ -70,6 +96,9 @@ def create_GT_masks(root_dir, background_dir, intrinsic_matrix, classes):
     #training_images_idx = load_obj(root_dir + "train_images_indices")
     #for i in range(len(training_images_idx)):
     for i, img_adr in enumerate(list_all_images):
+        # These are some bad ones for testing
+        #img_adr = "/home/alex/rit/Research/Datasets/DPOD/LineMOD_Dataset2/glue/data/color1022.jpg"
+        #img_adr = "/home/alex/rit/Research/Datasets/DPOD/LineMOD_Dataset2/glue/data/color444.jpg"
         #img_adr = list_all_images[training_images_idx[i]]
         label = os.path.split(os.path.split(os.path.dirname(img_adr))[0])[1]
         regex = re.compile(r'\d+')
@@ -79,7 +108,10 @@ def create_GT_masks(root_dir, background_dir, intrinsic_matrix, classes):
             print(str(i) + "/" + str(len(list_all_images)) + " finished!")
 
         image = cv2.imread(img_adr)
-        ID_mask = np.zeros((image.shape[0], image.shape[1]))
+        #print(img_adr)
+        #showImage("image", cv2.resize(image, (1600, 1200)))
+
+        ID_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
         U_mask = np.zeros((image.shape[0], image.shape[1]))
         V_mask = np.zeros((image.shape[0], image.shape[1]))
 
@@ -98,39 +130,41 @@ def create_GT_masks(root_dir, background_dir, intrinsic_matrix, classes):
         homogenous_coordinate = np.append(pt_cld_data[:, :3], ones, axis=1)
 
         # Perspective Projection to obtain 2D coordinates for masks
-        homogenous_2D = intrinsic_matrix @ (
-            rigid_transformation @ homogenous_coordinate.T)
+        homogenous_2D = intrinsic_matrix @ (rigid_transformation @ homogenous_coordinate.T)
         coord_2D = homogenous_2D[:2, :] / homogenous_2D[2, :]
         coord_2D = ((np.floor(coord_2D)).T).astype(int)
         x_2d = np.clip(coord_2D[:, 0], 0, 639)
         y_2d = np.clip(coord_2D[:, 1], 0, 479)
         ID_mask[y_2d, x_2d] = classes[label]
 
+        # Generate Ground Truth UV Maps
+        centre = np.mean(pt_cld_data, axis=0)
+        length = np.sqrt((centre[0]-pt_cld_data[:, 0])**2 + (centre[1] -
+                            pt_cld_data[:, 1])**2 + (centre[2]-pt_cld_data[:, 2])**2)
+        unit_vector = [(pt_cld_data[:, 0]-centre[0])/length, (pt_cld_data[:,
+                            1]-centre[1])/length, (pt_cld_data[:, 2]-centre[2])/length]
+        U = 0.5 + (np.arctan2(unit_vector[2], unit_vector[0])/(2*np.pi))
+        V = 0.5 - (np.arcsin(unit_vector[1])/np.pi)
+        U_mask[y_2d, x_2d] = U
+        V_mask[y_2d, x_2d] = V
+        # Convert float to uint8
+        U_mask = cv2.normalize(src=U_mask, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        V_mask = cv2.normalize(src=V_mask, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # Saving ID, U and V masks after using the fill holes function
+        ID_mask, U_mask, V_mask = fill_holes(ID_mask, U_mask, V_mask, classes[label])
+        cv2.imwrite(ID_mask_file, ID_mask)
+        cv2.imwrite(U_mask_file, U_mask)
+        cv2.imwrite(V_mask_file, V_mask)
+
         #if i % 100 != 0:  # change background for every 99/100 images
         # Generate the full set of background images so they're ready for use
         background_img_adr = os.path.join(background_dir, random.choice(os.listdir(background_dir)))
         background_img = cv2.imread(background_img_adr)
         background_img = cv2.resize(background_img, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_AREA)
-        background_img[y_2d, x_2d, :] = image[y_2d, x_2d, :]
+        background_img[ID_mask>0] = image[ID_mask>0]
         background_adr = os.path.join(root_dir, label,  "changed_background/color" + str(idx) + ".png")
-        mpimg.imsave(background_adr, background_img)
-
-        # Generate Ground Truth UV Maps
-        centre = np.mean(pt_cld_data, axis=0)
-        length = np.sqrt((centre[0]-pt_cld_data[:, 0])**2 + (centre[1] -
-                                                             pt_cld_data[:, 1])**2 + (centre[2]-pt_cld_data[:, 2])**2)
-        unit_vector = [(pt_cld_data[:, 0]-centre[0])/length, (pt_cld_data[:,
-                                                                          1]-centre[1])/length, (pt_cld_data[:, 2]-centre[2])/length]
-        U = 0.5 + (np.arctan2(unit_vector[2], unit_vector[0])/(2*np.pi))
-        V = 0.5 - (np.arcsin(unit_vector[1])/np.pi)
-        U_mask[y_2d, x_2d] = U
-        V_mask[y_2d, x_2d] = V
-
-        # Saving ID, U and V masks after using the fill holes function
-        ID_mask, U_mask, V_mask = fill_holes(ID_mask, U_mask, V_mask)
-        cv2.imwrite(ID_mask_file, ID_mask)
-        mpimg.imsave(U_mask_file, U_mask, cmap='gray')
-        mpimg.imsave(V_mask_file, V_mask, cmap='gray')
+        cv2.imwrite(background_adr, background_img)
 
 
 def create_UV_XYZ_dictionary(root_dir, classes):
